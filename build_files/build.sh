62b9497991a0a6710f110859705c07b6548a2824
vumast3r <kyuubi.2010@gmail.com>
2026-02-22 18:20:30 -0700

    refactor(build): rip out devel headers and use pre-compiled quickshell copr

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,8 +7,11 @@ echo "=== System Build Starting ==="
 # Material Symbols Font
 curl -Lo /etc/yum.repos.d/purian23-material-symbols-fonts.repo https://copr.fedorainfracloud.org/coprs/purian23/material-symbols-fonts/repo/fedora-$(rpm -E %fedora)/purian23-material-symbols-fonts-fedora-$(rpm -E %fedora).repo
 
-# Solopasha Hyprland (Provides the missing Hyprland packages)
+# Solopasha Hyprland
 curl -Lo /etc/yum.repos.d/solopasha-hyprland.repo https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-$(rpm -E %fedora)/solopasha-hyprland-fedora-$(rpm -E %fedora).repo
+
+# Quickshell Pre-Compiled Binaries
+curl -Lo /etc/yum.repos.d/errornointernet-quickshell.repo https://copr.fedorainfracloud.org/coprs/errornointernet/quickshell/repo/fedora-$(rpm -E %fedora)/errornointernet-quickshell-fedora-$(rpm -E %fedora).repo
 
 # 2. Install Hyprland Base Compositor
 rpm-ostree install \
@@ -30,16 +33,12 @@ rpm-ostree install \
     grim \
     slurp
 
-# 4. Install Qt6 & Wayland Compilation Headers
+# 4. Install Quickshell & Qt6 Runtime (NO DEVEL HEADERS REQUIRED)
 rpm-ostree install \
-    cmake ninja-build gcc-c++ python3-pip autoconf automake libtool \
-    fftw-devel alsa-lib-devel iniparser-devel qt6-qtbase-devel \
-    qt6-qtdeclarative-devel qt6-qtwayland-devel qt6-qtsvg-devel \
-    qt6-qtshadertools-devel wayland-devel wayland-protocols-devel \
-    cli11-devel spirv-tools pkgconf-pkg-config libdrm-devel \
-    mesa-libgbm-devel pipewire-devel pulseaudio-libs-devel \
-    aubio-devel libxkbcommon-devel pam-devel NetworkManager-libnm-devel \
-    lm_sensors-devel libqalculate-devel polkit-devel meson ncurses-devel \
-    qt6-qtdeclarative qt6-qt5compat qt6-qtsvg qt6-qtmultimedia
+    quickshell \
+    qt6-qtdeclarative \
+    qt6-qt5compat \
+    qt6-qtsvg \
+    qt6-qtmultimedia
 
 echo "=== System Build Complete ==="
